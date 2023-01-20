29cab47717275265cef9d69272e5b9af0a719daa
Patrik Moledo <moledop55@gmail.com>
2023-01-21 08:53:58 +0900

    part8: user authentication and permission (with permissions not working)

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Genre(models.Model):
@@ -57,6 +59,12 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -75,6 +83,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
