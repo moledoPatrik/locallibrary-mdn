827c6b53041f6bc154cb2cc1ed2240be898cb8c4
Patrik Moledo <moledop55@gmail.com>
2023-01-21 01:54:18 +0900

    part6: generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -22,3 +23,34 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    # to access page 2 -> /catalog/books/?page=2
+    paginate_by = 10
+
+    # changing default behaviour examples:
+    # context_object_name = 'book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+    # if we wanted to change some class model example:
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='Code')[:5]
+
+    # passing additional context variables to the template
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
